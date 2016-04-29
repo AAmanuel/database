faf247d6e74d159cf56c02b55b0ff4d710f6a3a8
Анна Амануел <Anna@macbook-pro-anna.local>
2016-04-29 19:55:53 +0300

    Initial Commit

---
+++ 4semestr/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  4semestr
+//
+//  Created by Анна Амануел on 29.04.16.
+//  Copyright © 2016 AMANUEL. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
