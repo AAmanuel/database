4fb8a5fb8519916c905fc72ddcfb3e94a84a28d7
Анна Амануел <Anna@macbook-pro-anna.local>
2016-05-02 04:53:06 +0300

    Add tests

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@
 #include "main.h"
 
 io_service service;
-ip::tcp::endpoint ep( ip::tcp::v4(), 1025); // слушаем порт 1025
+ip::tcp::endpoint ep( ip::tcp::v6(), 1025); // слушаем порт 1025
 ip::tcp::acceptor acc(service, ep); //обработчик входящих соединений
 
 void handle_accept(socket_ptr sock, const error_code & err)
